21340fdb8086b093ffb65c4780f595fcfcf485ad
Riwalenn Bas <riwalenn@gmail.com>
2024-03-03 11:56:41 +0100

    Lessons : @classmethod and @staticmethod

--- Lessons/Objects.py
+++ Lessons/Objects.py
@@ -1,29 +1,25 @@
 # Lessons:
 # - Objects
 # - Attributes and Methods
+# - Init
 class PlayerCharacter:
     membership = True
 
     def __init__(self, name, age):
-        if PlayerCharacter.membership:
-            self.name = name
-            self.age = age
-
-    def run(self):
-        print("run")
-        return 'done'
+        self.name = name
+        self.age = age
 
     def shout(self):
         print(f'my name is {self.name}')
 
+    @classmethod
+    def adding_things(cls, num1, num2):
+        return num1 + num2
 
-player1 = PlayerCharacter("Cindy", 38)
-player2 = PlayerCharacter("Tom", 21)
-player2.attack = 50
+    @staticmethod
+    def adding_things2(num1, num2):
+        return num1 + num2
 
-print(player1.run())
-print(player2.name)
-print(player2.age)
-print(player2.attack)
-print(player2.membership)
-print(player1.shout())
+
+# player1 = PlayerCharacter("Cindy", 38)
+print(PlayerCharacter.adding_things(3, 4))
