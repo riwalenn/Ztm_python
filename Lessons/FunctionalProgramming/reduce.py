38c1c3f8c2265f5d589296b963a3ba47385337c7
Riwalenn Bas <riwalenn@gmail.com>
2024-03-05 17:54:31 +0100

    Lessons : reduce()

---
+++ Lessons/FunctionalProgramming/reduce.py
@@ -0,0 +1,23 @@
+# Lessons:
+# - reduce()
+
+from functools import reduce
+
+my_list = [1, 2, 3]
+
+
+def multiply_by2(item):
+    return item * 2
+
+
+def only_odd(item):
+    return item % 2 != 0
+
+
+def accumulator(acc, item):
+    print(acc, item)
+    return acc + item
+
+
+print(reduce(accumulator, my_list, 0))
+print(my_list)
