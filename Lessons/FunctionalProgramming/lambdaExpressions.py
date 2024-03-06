59475c8c29eb363e9b789f1fa520f6110289c0e4
Riwalenn Bas <riwalenn@gmail.com>
2024-03-06 19:07:20 +0100

    Lessons : Lambda Expressions

---
+++ Lessons/FunctionalProgramming/lambdaExpressions.py
@@ -0,0 +1,13 @@
+# Lessons:
+# - Lambda Expressions
+
+
+from functools import reduce
+
+my_list = [1, 2, 3]
+
+
+print(list(map(lambda item: item*2, my_list)))
+print(list(filter(lambda item: item % 2 != 0, my_list)))
+print(reduce(lambda acc, item: acc+item, my_list))
+print(my_list)
