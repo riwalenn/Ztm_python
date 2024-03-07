a6bb4bad1eb932a1af5957b2de82501ebacbc73f
Riwalenn Bas <riwalenn@gmail.com>
2024-03-07 20:34:36 +0100

    Lessons : list Comprehensions

---
+++ Lessons/FunctionalProgramming/listComprehensions.py
@@ -0,0 +1,10 @@
+# Lessons:
+# - list Comprehensions
+# - Set and Dictionary
+
+my_list = [char for char in 'hello']
+my_list2 = [num for num in range(0,100)]
+my_list3 = [num**2 for num in range(0,100)]
+my_list4 = [num**2 for num in range(0,100) if num % 2 == 0]
+
+print(my_list4)
