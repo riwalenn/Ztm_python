d53aa27c5081973fd214222212886b1baeff07e4
Riwalenn Bas <riwalenn@gmail.com>
2024-03-08 20:10:36 +0100

    Lessons : Error Handling

---
+++ Lessons/Errors/errorHandling.py
@@ -0,0 +1,13 @@
+# Lessons:
+# - Error Handling
+
+while True:
+    try:
+        age = int(input('Enter your age: '))
+    except ValueError:
+        print('please enter an number')
+    except ZeroDivisionError:
+        print('please enter age higher than zero')
+    else:
+        print('thank you')
+        break
