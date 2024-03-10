07d778b24f32f2efbad5293312ff08d25bd7586d
Riwalenn Bas <riwalenn@gmail.com>
2024-03-10 09:55:56 +0100

    Lesson : how to debug code

---
+++ Lessons/debugging/debugging.py
@@ -0,0 +1,10 @@
+# Lessons
+# - debugging :
+#    - linting
+#    - ide / editor
+#    - read errors
+#    - pdb (python debugger, used to step through the code and debug) (use import pdb)
+#       pdb.set_trace()
+#       the interaction is in the terminal (Pdb) what I want to debug (ex: num1 or num2 in a method)
+#       you can use help (ex: help list)
+#    -
